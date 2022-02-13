f1085c7ea59a079dce16af3bc923fb3068de411f
artem5511 <artem_5511@mail.ru>
2022-02-13 16:30:57 +0300

    add 03_string

---
+++ 03_string.js
@@ -0,0 +1,51 @@
+// const name = 'Artem'
+// const age = 26
+
+// function getAge() {
+//     return age
+// }
+
+//const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + ' лет.'
+
+// const output = `Привет, меня зовут ${name} и мой возраст ${age < 35 ? 'A' : 'B'} лет.`
+// console.log(output)
+
+// const output = `
+//     <div>This is div</div>
+//     <p>this is p</p> `
+
+// console.log(output)
+
+// const name = 'Artem'
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.charAt(2))
+// console.log(name.indexOf('em'))
+// console.log(name.startsWith('art'))
+// console.log(name.toLowerCase().startsWith('art'))
+// console.log(name.startsWith('Art'))
+// console.log(name.endsWith('eM'))
+// console.log(name.repeat(3))
+// const string = '    password    '
+// console.log(string.trim())
+// console.log(string.trimLeft())
+// console.log(string.trimRight())
+
+// function logPerson(s, name, age) {
+//     //console.log(s, name, age)
+//     if (age < 0) {
+//         age = 'Еще не родился'
+//     }
+//     return  `${s[0]}${name}${s[1]}${age}${s[2]}`
+    
+// }
+// const personName = 'Артем'
+// const personAge = 32
+// const personName2 = 'Вася'
+// const personAge2 = -10
+
+// const output = logPerson `Имя: ${personName}, Возраст: ${personAge}!`
+// const output2 = logPerson `Имя: ${personName2}, Возраст: ${personAge2}!`
+// console.log(output)
+// console.log(output2)
